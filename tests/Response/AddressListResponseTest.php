935cc581f134a091ddb73dbc9ff1e929dc921634
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-11-19 08:20:34 +0100

    Added the AddressListResponse object

---
+++ tests/Response/AddressListResponseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigipolisGent\Tests\Flanders\BasicRegisters\Response;
+
+use DigipolisGent\Flanders\BasicRegisters\Response\AddressListResponse;
+use DigipolisGent\Flanders\BasicRegisters\Value\Address\Addresses;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DigipolisGent\Flanders\BasicRegisters\Response\AddressListResponse
+ */
+class AddressListResponseTest extends TestCase
+{
+    /**
+     * Response is created with Addresses collection.
+     *
+     * @test
+     */
+    public function responseHasAddressesCollection(): void
+    {
+        $addresses = new Addresses();
+        $response = new AddressListResponse($addresses);
+
+        $this->assertSame($addresses, $response->addresses());
+    }
+}
