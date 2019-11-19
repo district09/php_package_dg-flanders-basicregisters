8f4fec10c22fff9372c3aa638f60fced57ab542d
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-11-19 11:44:59 +0100

    Added the AddressDetailHandler

---
+++ src/Handler/AddressDetailHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigipolisGent\Flanders\BasicRegisters\Handler;
+
+use DigipolisGent\API\Client\Handler\HandlerInterface;
+use DigipolisGent\API\Client\Response\ResponseInterface;
+use DigipolisGent\Flanders\BasicRegisters\Normalizer\FromJson\Address\AddressDetailNormalizer;
+use DigipolisGent\Flanders\BasicRegisters\Request\AddressDetailRequest;
+use DigipolisGent\Flanders\BasicRegisters\Response\AddressDetailResponse;
+use Psr\Http\Message as Psr;
+
+/**
+ * Handles the list request.
+ */
+final class AddressDetailHandler implements HandlerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function handles(): array
+    {
+        return [AddressDetailRequest::class];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toResponse(Psr\ResponseInterface $response): ResponseInterface
+    {
+        $data = json_decode($response->getBody()->getContents());
+        $normalizer = new AddressDetailNormalizer();
+
+        return new AddressDetailResponse(
+            $normalizer->normalize($data)
+        );
+    }
+}
